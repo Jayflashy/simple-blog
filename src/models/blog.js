c9af600691ad5beb52ca8d54c1cc586fd6050e14
jayflashy <jadesdevelopers@gmail.com>
2022-10-03 07:29:35 +0100

    created blog model

---
+++ src/models/blog.js
@@ -0,0 +1,53 @@
+const mongoose = require('mongoose')
+
+const {Schema, model} = mongoose ;// import Schema & model
+
+// User Schema
+const BlogSchema = new Schema({
+    title: {
+        type: String,
+        required: [true, 'Blog title is required'],
+        minlength: [10, 'title must be more than 10 characters.'], 
+        trim:true
+    },
+    body: {
+        type: String,
+        required: [true, 'Blog body is required'],
+        minlength: [10, 'body must be more than 10 characters.'], 
+    },
+    slug: {
+        type: String,
+        required: [true, 'slug is required'],
+        unique: true,
+        trim:true
+    },
+    category: {
+        type: String, 
+        trim:true
+    },
+    tags: {
+        type: String,
+        trim:true
+    },
+    image: {
+        type: String,
+    },
+    isAdmin: {
+        type: Boolean,
+        default: false
+    },
+    active: {
+        type: Boolean,
+        default: true,
+        select: false
+    }
+},
+{
+    timestamps: {
+    }
+})
+
+// User model
+const Blog = model("Blog", BlogSchema)
+
+module.exports = Blog
